2fc9a727dfe8e3abb4c40d15434a492971b57e74
amit pokhrel <contact.amitpok@gmail.com>
2022-06-12 18:00:49 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp( MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              height: 250,
+              width: 250,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('images/bulb.jpg'),
+                ),
+                border: Border.all(
+                  color: Colors.orange,
+                  width: 10,
+                ),
+                borderRadius: BorderRadius.circular(150),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
