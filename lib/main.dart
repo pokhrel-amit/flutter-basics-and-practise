e7ce4d9ae0a4ddb402a5d57efcbf7ea05fd4b4da
amit pokhrel <contact.amitpok@gmail.com>
2022-06-13 16:08:01 +0545

    feat:bulb app using stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+// Light Bulb App
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyBulbApp();
+  }
+}
+
+class MyBulbApp extends StatefulWidget {
+  const MyBulbApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyBulbApp> createState() => _MyBulbAppState();
+}
+
+class _MyBulbAppState extends State<MyBulbApp> {
+  bool isTurnedOn = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    print('text button presses!');
-                  },
-                  onLongPress: () {
-                    print('long press button!');
-                  },
-                  child: const Text(
-                    'Text Button',
-                    style: TextStyle(
-                      color: Colors.red,
-                      backgroundColor: Colors.yellow,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
+        appBar: AppBar(
+          title: const Text(
+            'Light Bulb',
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+               isTurnedOn? Icons.lightbulb: Icons.lightbulb_outline,
+                size: 200,
+                color: isTurnedOn ? Colors.yellow : Colors.black,
+                
+              ),
+              ElevatedButton(
                   onPressed: (() {
-                    print('Elevated button');
+                    setState(() {
+                      isTurnedOn = !isTurnedOn;
+                    });
                   }),
-                  child: const Text(
-                    'Elevated BUTTON',
+                  child: Text(
+                    isTurnedOn ? 'Turn Off' : 'Turn On',
                     style: TextStyle(
-                      color: Colors.black,
+                      backgroundColor: Colors.lightBlue.withOpacity(0.3),
+                      fontSize: 30,
+                      fontFamily: 'DancingScript',
+                      fontWeight: FontWeight.w900,
                     ),
-                  ),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                    Colors.grey,
-                  )),
-                ),
-                ElevatedButton(
-                  onPressed: (() {
-                    print('Elevated button 2');
-                  }),
-                  child: const Text(
-                    'Elevated BUTTON 2',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith(
-                      (states) => states.contains(MaterialState.pressed)
-                          ? Colors.brown
-                          : Colors.yellow,
-                    ),
-                  ),
-                ),
-                OutlinedButton(
-                  onPressed: (() {
-                    print('outlined button ');
-                  }),
-                  child: const Text(
-                    'Outlined BUTTON ',
-                  ),
-                  style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all(
-                    Colors.teal,
-                  )),
-                ),
-                IconButton(
-                  onPressed: () {
-                    print('Icon Button');
-                  },
-                  icon: const Icon(
-                    Icons.volume_down_rounded,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                )
-              ],
-            ),
+                  ))
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.black,
-          splashColor: Colors.indigoAccent,
-          onPressed: (() {
-            print('Floating button pressed!');
-          }),
-          child: const Icon(
-            Icons.add,
-            // color: Colors.yellow
-          ),
-        ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
-
     );
   }
 }
