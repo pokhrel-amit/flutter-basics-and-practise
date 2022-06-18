750a49896cea7e00d5ee973deff9a71861dd73eb
amit pokhrel <contact.amitpok@gmail.com>
2022-06-18 16:17:24 +0545

    context passed

--- lib/score_card.dart
+++ lib/score_card.dart
@@ -1,25 +1,29 @@
+
 // import 'package:firstapp/custom_card.dart';
 import 'package:firstapp/custom_card.dart';
+import 'package:firstapp/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:firstapp/main.dart';
 
 class ScoreCard extends StatelessWidget {
-  ScoreCard({this.currentScore = 0, this.totalAttempted = 0});
-  int currentScore;
-  int totalAttempted;
+   const ScoreCard({required this.currentScore ,required this.totalAttempted,required this.context });
+  final int currentScore;
+  final int totalAttempted; 
+  final context;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context ) {
+    ThemeData theme = Theme.of(this.context);
     return CustomCard(
       color: Colors.blue.shade400,
-      elevation: 20,
+      // elevation: 20,
       height: 60,
       shapeRadius: 20,
-      headingWidget: Text(
+      headingWidget: Text( 
+        
         'Score: $currentScore / $totalAttempted',
-        style: const TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.w800,
-        ),
+        style: theme.textTheme.headline2,
+        
       ),
     );
   }
